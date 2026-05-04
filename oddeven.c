839eb21a424287128074305f6c9c10dca2bd42ae
adityasharma <sharmaaditya46486@gmail.com>
2026-05-04 13:53:03 +0530

    added even odd program

---
+++ oddeven.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int num;
+    printf("ENTER A NUMBER = ");
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("numer is even");
+    }
+    else{
+        printf("number is odd");
+    }
+return 0;
+    }
