1c85e15ccf7899046e696247f8d3348abc962040
adityasharma <sharmaaditya46486@gmail.com>
2026-05-05 16:12:48 +0530

    this is my table code using while loop in c

---
+++ table1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,n;
+    printf("ENTER NUM WHOSE U WANT TABLE: ");
+    scanf("%d",&n);
+    a = 1;
+    while(a<=10){
+        printf("%d * %d = %d\n",n,a,n*a);
+        a++;
+    }
+}
