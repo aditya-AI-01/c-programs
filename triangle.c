93e377dd681f45fc17bd1165a84dc9e81f04a3d1
adityasharma <sharmaaditya46486@gmail.com>
2026-05-07 21:56:34 +0530

    this is my all basic star patterns of right angled triangles (4)in c

---
+++ triangle.c
@@ -0,0 +1,60 @@
+/* #include <stdio.h>
+int main(){
+    int a,b;
+    for(a=1;a<=5;a++){
+    for(b=1;b<=a;b++){
+    printf("*");
+    }
+    printf("\n");
+    }
+    return 0;
+} */
+/* /*
+#include <stdio.h>
+int main(){
+    int a,b;
+    for(a=5;a>=1;a--){
+    for(b=1;b<=a;b++){
+        printf("*");
+    }
+    printf("\n");
+    }
+    return 0;
+}*/
+     
+
+/*#include <stdio.h>
+int main(){
+    int a,b;
+    for(a=1;a<=5;a++){
+    for(b=1;b<=5;b++){
+    if(a+b>=6){
+        printf("*");}
+        else{
+            printf(" ");
+        }   
+    }    
+    printf("\n");
+    }
+    return 0;
+}
+*/
+/**
+#include <stdio.h>
+int main(){
+    int a,b;
+    for(a=5;a>=1;a--){
+    for(b=1;b<=5;b++){
+    if(a+b>=6){
+    printf("*");
+    }
+    else{
+    printf(" ");
+    }
+    }
+    printf("\n");
+    }
+    return 0;
+    }*/
+
+    
